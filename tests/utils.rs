43f4a47e6137821d117b005071961e3f401365de
revil <revil.com@gmail.com>
2025-09-24 19:53:41 +0900

    format: hook

--- tests/utils.rs
+++ tests/utils.rs
@@ -2,47 +2,38 @@
 #![allow(unused)]
 
 // original: merge()
-mod merge {
-}
+mod merge {}
 
 // original: assign()
-mod assign {
-}
+mod assign {}
 
 // original: combine()
 mod combine {
     // original: both arrays
     #[test]
-    fn both_arrays() {
-    }
+    fn both_arrays() {}
 
     // original: one array, one non-array
     #[test]
-    fn one_array_one_non_array() {
-    }
+    fn one_array_one_non_array() {}
 
     // original: neither is an array
     #[test]
-    fn neither_is_an_array() {
-    }
+    fn neither_is_an_array() {}
 }
 
 // original: decode
-mod decode {
-}
+mod decode {}
 
 // original: encode
 mod encode {
     // original: symbols
     #[test]
-    fn symbols() {
-    }
+    fn symbols() {}
 }
 
 // original: isBuffer()
-mod isbuffer {
-}
+mod isbuffer {}
 
 // original: isRegExp()
-mod isregexp {
-}
+mod isregexp {}
